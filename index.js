8b89cdb6bcf3b2e673f289e6bc4f0dbb672f05f2
Rob Raisch <rraisch@yieldbot.com>
2015-04-21 17:41:24 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
+/* Created by raisch on 4/21/15. */
 
-var _=root._ || require('lodash');
+/*jshint node:true, bitwise:true, camelcase:false, curly:true, undef:false, unused:false, eqeqeq:true, shadow:true */
+
+'use strict';
+
+var _ = require('lodash');
+
+/**
+ * Returns a new object composed of specified paths.
+ * @param {object} data - object from which to extract values at paths
+ * @param {string|array<string>} paths - path or list of paths to extract
+ * @param {string} [delim='.'] - used to split paths
+ * @returns {object}
+ *
+ * @example
+ *
+ *   var data={ a: { b: { c: 1, d: 2 }, e: { f: 3, g: 4 } }, h: 5 },
+ *       paths=['a.b.c','a.e.g','h'];
+ *
+ *   sift(data,paths) => { a: { b: { c: 1 }, e: { g: 4 } }, h: 5 }
+ */
+var sift = function (obj, paths, delim) {
+  paths = _.isArray(paths) ? paths : [paths];
+  delim = delim || '.';
+
+  if (!_.isObject(obj)) {
+    throw new Error('requires an object');
+  }
+  if (!_.isArray(paths)) {
+    throw new Error('requires a path or array of paths');
+  }
+
+  return _.map(paths, function (path) {
+    var result = {},
+        curr_result = result,
+        curr_obj = obj;
+
+    _.map(path.split(delim), function (part) {
+      if (_.isArray(curr_obj)) {
+        curr_obj = curr_obj[part];
+      }
+      else {
+        curr_obj = curr_obj[part];
+        curr_result[part] = _.isObject(curr_obj) ? {} : curr_obj;
+        curr_result = curr_result[part];
+      }
+    });
+    return result;
+  }).reduce(_.merge);
+};
 
 _.mixin({
-  paths:function(obj,paths,delim){
-  }
+  sift: sift
 });
+
+module.exports=_;
